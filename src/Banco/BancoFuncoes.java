cc4691ffca2a1603f4ae2815610bc13e3ec983a2
Paulo <Paulo@MasterRacer>
2015-09-12 14:02:39 -0300

    metodo para preencher a tabela funcionario funcionando

--- src/Banco/BancoFuncoes.java
+++ src/Banco/BancoFuncoes.java
@@ -148,4 +148,76 @@ public class BancoFuncoes {
     }
 
  }
+           
+                 public BancoFuncoes
+           tabelaFuncionario(String sql, String msg1, String msg2, final JTable gridFuncionario){
+
+               
+               
+          Connection connection = null;
+
+   try {
+
+        Class.forName(cb.JDBC_DRIVER()).newInstance();
+        connection = (Connection) DriverManager.getConnection(cb.DB_URL(), cb.DB_USER(), cb.DB_PASS());
+
+        Statement s = (Statement) connection.createStatement();
+
+          ResultSet rs = s.executeQuery(sql);
+
+          javax.swing.table.DefaultTableModel dtm = (javax.swing.table.DefaultTableModel)gridFuncionario.getModel();
+                   int i = dtm.getRowCount();
+                   for(int a = 0; a < i; a++)
+                   {
+                   dtm.removeRow(0);
+                   }
+
+                   while (rs.next()){
+                       String Nome = rs.getString("nomeFun");
+                        String Sexo = rs.getString("sexoFun");
+                        String CPF = rs.getString("cpfFun");
+                        String RG = rs.getString("rgFun");
+                        String Data_Nascimento = rs.getString("nasciFun");
+                        String UF = rs.getString("ufFun");
+                        String Cidade = rs.getString("cidadeFun");
+                        String Endereco = rs.getString("endeFun");
+                        String Bairro = rs.getString("bairroFun");
+                        String Cep = rs.getString("cepFun");
+                        String Numero = rs.getString("numFun");
+                        String Complemento = rs.getString("compleFun");
+                        String Telefone = rs.getString("telFun");
+                        String Ceular = rs.getString("celFun");
+                        String Setor = rs.getString("setorFun");
+             
+                        dtm.addRow(new Object[]{Nome ,Sexo ,CPF,RG ,Data_Nascimento , UF,Cidade ,Endereco ,Bairro ,Cep , Numero, Complemento,Telefone ,Ceular , Setor});
+                   }
+
+
+//        System.out.println("Fornecedores Recuperados com sucesso");
+
+         s.close();
+         connection.close();
+
+         System.out.println(msg1);
+         System.out.println(sql);
+         return banco;
+
+   }catch(SQLException ex){
+      JOptionPane.showMessageDialog(null, msg2);
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("Error: " + ex.getErrorCode());
+      System.out.println(sql);
+      return null;
+    }
+    catch(Exception e){
+      JOptionPane.showMessageDialog(null, msg2);
+      System.out.println(sql);
+      System.out.println("Problemas ao tentar conectar com o banco de dados: " + e);
+      return null;
+    }
+
+ }
+           
+           
 }
